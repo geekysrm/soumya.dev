f91766a1eaf39eddb7b6b13b8cadf477fdeed7d7
geekysrm <soumyacool2012@gmail.com>
2020-07-07 14:04:58 +0530

    update styles of decode page

--- src/pages/decode.js
+++ src/pages/decode.js
@@ -1,5 +1,6 @@
 // Base-64 and ROT13 decoder
 import { useState } from "react";
+import Head from "next/head";
 import axios from "axios";
 
 const ROT13_API_URL = "https://helloacm.com/api/rot13/?s="; //GET
@@ -37,37 +38,99 @@ export default function Decode() {
   };
   return (
     <div>
-      <span>Welcome to decoder</span>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Soumya's Decoder</title>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
+        ></script>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Lora"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Josefin+Sans"
+          rel="stylesheet"
+        ></link>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div>
-        <span>Select type</span>
-        <br />
-        <select
-          value={decodeType}
-          onChange={(e) => setDecodeType(e.target.value)}
-        >
-          <option value="rot13">ROT13</option>
-          <option value="base64">Base64</option>
-        </select>
-        <br />
-        <label>
-          Encoded Text:
-          <input
-            type="text"
-            name="encodedText"
-            value={encodedText}
-            onChange={(e) => setEncodedText(e.target.value)}
-          />
-        </label>
-        <button onClick={handleDecode}>Decode</button>
-        <br />
-        {decodedText && (
-          <>
-            <span>Decoded Text is:</span>
-            <br />
-            <span>{decodedText}</span>
-          </>
-        )}
+        <h2 className="header">Welcome to decoder</h2>
+        <div className="form">
+          <h4>Select type</h4>
+          <div className="form-group">
+            <select
+              className="form-control"
+              value={decodeType}
+              onChange={(e) => setDecodeType(e.target.value)}
+            >
+              <option value="rot13">ROT13</option>
+              <option value="base64">Base64</option>
+            </select>
+          </div>
+          <br />
+          <label>
+            <h4>Encoded Text:</h4>
+            <input
+              className="form-control encoded-input"
+              type="text"
+              name="encodedText"
+              value={encodedText}
+              onChange={(e) => setEncodedText(e.target.value)}
+            />
+          </label>
+          <br />
+          <button className="btn btn-info" onClick={handleDecode}>
+            Decode
+          </button>
+          <br />
+          <br />
+          {decodedText && (
+            <>
+              <h4>Decoded Text is:</h4>
+              <div className="decoded-text alert alert-secondary">
+                {decodedText}
+              </div>
+            </>
+          )}
+          {/*Add copy to clipboard button*/}
+        </div>
       </div>
+      <style jsx>{`
+        .header {
+          margin-top: 10px;
+          text-align: center;
+        }
+        select {
+          width: 15%;
+        }
+        .form {
+          padding: 0px 100px;
+        }
+        .encoded-input {
+          width: 210%;
+        }
+        .btn {
+          margin-top: 10px;
+        }
+        .decoded-text {
+          display: inline-block;
+        }
+      `}</style>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>Soumya Ranjan Mohanty</title>
-        <link rel="stylesheet" href="css/style.css" />
         <script
           defer
           src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
