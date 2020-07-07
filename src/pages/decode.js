28b54e8f8c792c00e8465dc6a2de4c62f9553e08
geekysrm <soumyacool2012@gmail.com>
2020-07-07 13:14:28 +0530

    add decode page for htb post

--- src/pages/decode.js
+++ src/pages/decode.js
@@ -1,11 +1,34 @@
 // Base-64 and ROT13 decoder
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const ROT13_API_URL = "https://helloacm.com/api/rot13/?s="; //GET
 
 export default function Decode() {
-  const handleDecode = () => {
+  const [decodeType, setDecodeType] = useState("rot13");
+  const [encodedText, setEncodedText] = useState("");
+  const [decodedText, setDecodedText] = useState("");
+
+  const handleDecode = async () => {
     console.log("handleDecode button clicked");
-    // if ROT13 make API call
-    // if base64 do base64 decoding, e.g. const decodedData = window.atob(encodedData);
+    console.log(decodeType);
+
+    if (decodeType === "rot13") {
+      // make API call to decode rot13
+      try {
+        const res = await axios.get(`${ROT13_API_URL}${encodedText}`);
+        console.log("rot13 decoded", res.data);
+        setDecodedText(res.data);
+      } catch (error) {
+        console.error("Some error occured while decoding", error);
+        setDecodedText("");
+      }
+    } else if (decodeType === "base64") {
+      // do base64 decoding, e.g.const decodedData = window.atob(encodedData);
+      const base64DecodedText = window.atob(encodedText);
+      console.log("base64 decoded", base64DecodedText);
+      setDecodedText(base64DecodedText);
+    }
   };
   return (
     <div>
@@ -13,24 +36,32 @@ export default function Decode() {
       <div>
         <span>Select type</span>
         <br />
-        <input
-          type="radio"
-          id="rot13"
-          name="decode-type"
-          defaultValue="rot13"
-          checked
-        />
-        <label htmlFor="rot13">ROT13</label>
+        <select
+          value={decodeType}
+          onChange={(e) => setDecodeType(e.target.value)}
+        >
+          <option value="rot13">ROT13</option>
+          <option value="base64">Base64</option>
+        </select>
         <br />
-        <input
-          type="radio"
-          id="base64"
-          name="decode-type"
-          defaultValue="base64"
-        />
-        <label htmlFor="base64">Base64</label>
-        <br />
+        <label>
+          Encoded Text:
+          <input
+            type="text"
+            name="encodedText"
+            value={encodedText}
+            onChange={(e) => setEncodedText(e.target.value)}
+          />
+        </label>
         <button onClick={handleDecode}>Decode</button>
+        <br />
+        {decodedText && (
+          <>
+            <span>Decoded Text is:</span>
+            <br />
+            <span>{decodedText}</span>
+          </>
+        )}
       </div>
     </div>
   );
