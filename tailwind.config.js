9851e677d5305b9f5baf2e532592af71cc34ea2b
geekysrm <soumyacool2012@gmail.com>
2020-07-09 21:41:31 +0530

    add tailwind config stuff

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
