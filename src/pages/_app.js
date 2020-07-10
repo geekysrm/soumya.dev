5bad5acffb6e67040b3b086b4daed3912e1b7f23
geekysrm <soumyacool2012@gmail.com>
2020-07-10 18:50:06 +0530

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
 import "../styles/index.css";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
