0c057edb4eb97938d7af28e58ddf6f6eb6daaa65
geekysrm <soumyacool2012@gmail.com>
2020-07-13 00:24:41 +0530

    test jsx inside mdx

---
+++ src/components/Button.js
@@ -0,0 +1,4 @@
+const Button = () => (
+  <button className="px-4 py-2 font-light bg-green-400">Click Me</button>
+);
+export default Button;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,5 @@
 import { parseISO, format } from "date-fns";
+import readingTime from "reading-time";
 
 // To add layout for blog
 // show title, author(me) with photo, date, time to read, nProgressbar, (views?)
@@ -6,6 +7,7 @@ import { parseISO, format } from "date-fns";
 export default function Layout(frontMatter) {
   return ({ children: content }) => {
     console.log(frontMatter);
+    // console.log("readingTime", frontMatter.readingTime.text);
     return (
       <div>
         <h1 className="text-3xl font-bold">{frontMatter.title}</h1>
