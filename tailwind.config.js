152956860eed555239e1ec2ca0389e2e7d159d82
geekysrm <soumyacool2012@gmail.com>
2020-07-26 02:49:15 +0530

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: [
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
