a2b6e25253d195dc8c6e8c7e31373be5d59c888a
geekysrm <soumyacool2012@gmail.com>
2020-07-27 19:24:58 +0530

    move author info to separate component

---
+++ src/components/AuthorCard.js
@@ -0,0 +1,81 @@
+import Link from "next/link";
+
+export default function AuthorCard() {
+  return (
+    <aside className="flex-1 h-full px-5 py-3 bg-yellow-200 md:ml-4 md:block">
+      <h3 className="text-xl font-semibold">Author</h3>
+      <div>
+        <div>
+          <img
+            className="w-24 mt-3 rounded-full"
+            alt="Soumya Ranjan Mohanty (geekySRM)"
+            src="/static/images/soumya.jpg"
+          />
+        </div>
+        <p className="mt-3">
+          I'm{" "}
+          <Link href="/">
+            <a>Soumya</a>
+          </Link>
+          , a software engineer and open-sourcer. I am a{" "}
+          <a
+            href="https://www.credential.net/9h8314eo?key=219f5a29de9f98ac6d2def0a89a0a38c7f1ffe5e"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google certified MWS
+          </a>{" "}
+          and I like to tinker around with new technologies,{" "}
+          <Link href="/blog">
+            <a>write</a>
+          </Link>{" "}
+          about them and build{" "}
+          <a
+            href="https://github.com/geekysrm?tab=repositories&type=source"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            projects
+          </a>{" "}
+          with them.
+        </p>
+        {/* TODO: Add newsletter form */}
+        {/* <div className="px-0 mt-3 bg-gray-100">
+                <NewsLetterForm />
+              </div> */}
+        <div className="flex items-center mt-3">
+          <img
+            className="w-6 ml-2"
+            src="/static/images/email.svg"
+            alt="Buy me a coffee"
+          />
+          <a
+            className="ml-4"
+            href="https://tinyletter.com/geekysrm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Subscribe to my newsletter
+          </a>
+        </div>
+        <div className="flex items-center mt-3">
+          <img
+            className="w-10 -mt-2"
+            src="/static/images/coffee.svg"
+            alt="Buy me a coffee"
+          />
+          <a
+            className="ml-2"
+            href="https://coffee.soumya.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Buy me a coffee
+          </a>
+        </div>
+        {/* TODO: Add Subscribe to RSS */}
+        {/* TODO: add Search anything in this site field */}
+      </div>
+    </aside>
+  );
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,7 @@
 // Layout for blog post
 import { parseISO, format } from "date-fns";
-import Link from "next/link";
 import Head from "next/head";
 import getShareImage from "@jlengstorf/get-share-image";
-
-import Header from "../components/Header";
-import Utterances from "../components/Utterances";
-import NewsLetterForm from "../components/NewsLetterForm";
-import IconClock from "../components/SVGIcons/IconClock";
-import IconEdit from "../components/SVGIcons/IconEdit";
-import MetaTags from "../components/MetaTags";
 import {
   FacebookShareButton,
   LinkedinShareButton,
@@ -18,6 +10,14 @@ import {
   FacebookIcon,
   LinkedinIcon,
 } from "react-share";
+
+import Header from "../components/Header";
+import Utterances from "../components/Utterances";
+import NewsLetterForm from "../components/NewsLetterForm";
+import IconClock from "../components/SVGIcons/IconClock";
+import IconEdit from "../components/SVGIcons/IconEdit";
+import MetaTags from "../components/MetaTags";
+import AuthorCard from "../components/AuthorCard";
 
 // show nProgressbar, (views?)
 // more SEO stuff to be added if needed
@@ -81,82 +81,7 @@ export default function Layout(frontMatter) {
             {/* Add more responsive styles to {content} below */}
             <div className="mt-5 prose lg:prose-lg">{content}</div>
           </div>
-          {/* TODO: Display author info in other way on screens smaller than below breakpoint */}
-          <aside className="flex-1 h-full px-5 py-3 bg-yellow-200 md:ml-4 md:block">
-            <h3 className="text-xl font-semibold">Author</h3>
-            <div>
-              <div>
-                <img
-                  className="w-24 mt-3 rounded-full"
-                  alt="Soumya Ranjan Mohanty (geekySRM)"
-                  src="/static/images/soumya.jpg"
-                />
-              </div>
-              <p className="mt-3">
-                I'm{" "}
-                <Link href="/">
-                  <a>Soumya</a>
-                </Link>
-                , a software engineer and open-sourcer. I am a{" "}
-                <a
-                  href="https://www.credential.net/9h8314eo?key=219f5a29de9f98ac6d2def0a89a0a38c7f1ffe5e"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Google certified MWS
-                </a>{" "}
-                and I like to tinker around with new technologies,{" "}
-                <Link href="/blog">
-                  <a>write</a>
-                </Link>{" "}
-                about them and build{" "}
-                <a
-                  href="https://github.com/geekysrm?tab=repositories&type=source"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  projects
-                </a>{" "}
-                with them.
-              </p>
-              {/* TODO: Add newsletter form */}
-              {/* <div className="px-0 mt-3 bg-gray-100">
-                <NewsLetterForm />
-              </div> */}
-              <div className="flex items-center mt-3">
-                <img
-                  className="w-6 ml-2"
-                  src="/static/images/email.svg"
-                  alt="Buy me a coffee"
-                />
-                <a
-                  className="ml-4"
-                  href="https://tinyletter.com/geekysrm"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Subscribe to my newsletter
-                </a>
-              </div>
-              <div className="flex items-center mt-3">
-                <img
-                  className="w-10 -mt-2"
-                  src="/static/images/coffee.svg"
-                  alt="Buy me a coffee"
-                />
-                <a
-                  className="ml-2"
-                  href="https://coffee.soumya.dev"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Buy me a coffee
-                </a>
-              </div>
-              {/* TODO: Add Subscribe to RSS */}
-              {/* TODO: add Search anything in this site field */}
-            </div>
-          </aside>
+          <AuthorCard />
         </article>
         {/* Comments below */}
         {/* <hr className="mt-12 mb-4" /> */}
