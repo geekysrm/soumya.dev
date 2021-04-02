be907c4a34512b4ca71f5865196ce36b595d1b3f
geekysrm <soumyacool2012@gmail.com>
2021-04-02 21:44:28 +0530

    Add react-icons and social icons on hero section

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,5 +1,15 @@
 // New homepage at /
 import { motion } from "framer-motion";
+import {
+  FaProductHunt,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+  FaYoutube,
+  FaTwitch,
+  FaDev,
+  FaMedium,
+} from "react-icons/fa";
 
 export default function NewSite() {
   return (
@@ -23,15 +33,73 @@ export default function NewSite() {
             👋
           </motion.div>
         </h1>
-        <p className="mt-2 text-lg">
-          Software Developer building useful and accessible applications.
-        </p>
+        <div className="mt-6">
+          <p className="text-2xl">
+            A Software Developer from India bringing{" "}
+            <span className="text-red-500">ideas</span> to life.
+          </p>
+          <p>
+            You can read more <a>about me</a> or have a look at my{" "}
+            <a>projects</a>.
+          </p>
+        </div>
         <a
           href="mailto:hey@soumya.dev"
-          className="inline-block px-6 py-2 mt-4 font-bold text-white bg-blue-500 border-gray-600"
+          className="inline-block px-6 py-2 mt-4 font-bold text-white bg-red-500 border-gray-600"
         >
           Say Hello
         </a>
+        <div className="flex mt-5 space-x-5">
+          {/* Socials - add/remove links */}
+          <a
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaTwitter className="text-3xl text-blue-500 opacity-70" />
+          </a>
+          <a
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaProductHunt className="text-3xl text-red-700 opacity-70" />
+          </a>
+          <a
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaGithub className="text-3xl text-black-500 opacity-70" />
+          </a>
+          <a
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaLinkedin className="text-3xl text-blue-500 opacity-70" />
+          </a>
+          <a
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaYoutube className="text-3xl text-red-500 opacity-70" />
+          </a>
+          <a
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaTwitch className="text-3xl text-purple-500 opacity-70" />
+          </a>
+          <a>
+            <FaDev className="text-3xl text-black opacity-70" />
+          </a>
+          <a>
+            <FaMedium className="text-3xl text-black opacity-70" />
+          </a>
+        </div>
       </section>
     </div>
   );
