15dad128d54e0108ef524737269ab3fc2831a97d
geekysrm <soumyacool2012@gmail.com>
2021-04-02 20:47:00 +0530

    Add framer-motion and some animations

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,9 +1,28 @@
 // New homepage at /
+import { motion } from "framer-motion";
+
 export default function NewSite() {
   return (
     <div>
       <section className="px-4 pt-20">
-        <h1 className="text-5xl font-extrabold">Hi, I'm Soumya 👋</h1>
+        <h1 className="flex text-5xl font-extrabold">
+          Hi, I'm Soumya
+          <motion.div
+            className="ml-2"
+            animate={{
+              rotate: [0.0, 14.0, -8.0, 14.0, -4.0, 10.0, 0.0, 0.0],
+            }}
+            transition={{
+              duration: 2.5,
+              ease: "easeInOut",
+              times: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1],
+              loop: Infinity,
+              repeatDelay: 1,
+            }}
+          >
+            👋
+          </motion.div>
+        </h1>
         <p className="mt-2 text-lg">
           Software Developer building useful and accessible applications.
         </p>
